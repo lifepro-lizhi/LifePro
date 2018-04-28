e443315955d208ecd53ad807c50122686d9c1dd9
Li Zhi <powerflame426@gmail.com>
2018-04-28 21:15:54 +0800

    Comment模块的parent TreeForeignKey增加on_delete

--- Comment/models.py
+++ Comment/models.py
@@ -27,7 +27,8 @@ class Comment(MPTTModel):
     create_date = models.DateField(auto_now=True)
     # parent = models.ForeignKey("self", blank=True, null=True)
     parent = TreeForeignKey('self', null=True, blank=True,
-                            related_name='children', db_index=True)
+                            related_name='children', db_index=True,
+                            on_delete=models.CASCADE)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
