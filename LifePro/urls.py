305cd18d5b6d2348e48a12aebd867e08a79154f7
Li Zhi <powerflame426@gmail.com>
2018-04-30 09:03:38 +0800

    修改界面

--- LifePro/urls.py
+++ LifePro/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     url(r'^like/', include('Like.urls')),
     url(r'^$', views.BlogListView.as_view(), name='home'),
     url(r'^user/', include('Users.urls'), name='users'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_ddroot=settings.STATIC_ROOT)
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_ddroot=settings.STATIC_ROOT)
